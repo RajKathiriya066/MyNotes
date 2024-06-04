058afa6598771ecbbb64c154a96153c216c38eb4
RK066 <rajkathiriya66@gmail.com>
2024-06-05 01:08:35 +0530

    added files for auth service

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,16 @@
+//login exception
+class UserNotFoundAuthException implements Exception {}
+
+class WrongPassAuthException implements Exception {}
+
+//register exception
+class WeakPassAuthException implements Exception {}
+
+class EmailAlreadyInUse implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
+
+//generic exception
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
