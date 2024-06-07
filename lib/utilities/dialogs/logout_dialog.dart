9ab12292d90b52d6b97d55d0c2738dd8a2899589
RK066 <rajkathiriya66@gmail.com>
2024-06-08 03:33:35 +0530

    created generic class for dialogs

---
+++ lib/utilities/dialogs/logout_dialog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:mynotes/utilities/dialogs/generic_dialog.dart';
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showGenericDialog(
+      context: context,
+      title: "Log out",
+      content: "Are you sure you want to log out?",
+      optionBuilder: () => {
+            'No': false,
+            'Yes': true,
+          }).then(
+    (value) => value ?? false,
+  );
+}
---
+++ lib/views/notes/notes_view.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/services/crud/notes_service.dart';
+
+import '../enums/menu_action.dart';
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  late final NotesService _notesService;
+  String get userEmail => AuthService.firebase().currentUser!.email!;
+
+  @override
+  void initState() {
+    _notesService = NotesService();
+    _notesService.open();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _notesService.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("notes"), actions: [
+        PopupMenuButton<MenuAction>(
+          onSelected: (value) async {
+            switch (value) {
+              case MenuAction.logout:
+                final shouldLogout = await showLogOutDialog(context);
+                if (shouldLogout) {
+                  await AuthService.firebase().logout();
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil(loginRoute, (route) => false);
+                }
+                break;
+            }
+          },
+          itemBuilder: (context) {
+            return const [
+              PopupMenuItem<MenuAction>(
+                value: MenuAction.logout,
+                child: Text("logout"),
+              )
+            ];
+          },
+        )
+      ]),
+      body: FutureBuilder(
+        future: _notesService.getOrCreateUser(email: userEmail),
+        builder: (context, snapshot) {
+          switch(snapshot.connectionState){
+
+            case ConnectionState.done:
+              return StreamBuilder(stream: _notesService.allNotes, builder: (context,snapshot){
+                switch(snapshot.connectionState){
+                  case ConnectionState.waiting:
+                    return const Text('waiting for all notes...');
+                  default:
+                    return const CircularProgressIndicator();
+                }
+              });
+            default:
+              return CircularProgressIndicator();
+          }
+        },
+      ),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Log out'),
+          content: const Text('Are you sure you want to log out?'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text('Yes')),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text('No'))
+          ],
+        );
+      }).then((value) => value ?? false);
+}
