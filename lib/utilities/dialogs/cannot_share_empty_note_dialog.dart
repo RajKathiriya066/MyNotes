9588245f2d646b7ddc94ba617442b068ba9978d5
RK066 <rajkathiriya66@gmail.com>
2024-06-18 03:34:43 +0530

    enabled sharing note

---
+++ lib/utilities/dialogs/cannot_share_empty_note_dialog.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:mynotes/utilities/generics/generic_dialog.dart';
+
+Future<void> showCannotShareEmptyNoteDialog(BuildContext context) {
+  return showGenericDialog<void>(
+      context: context,
+      title: "Sharing",
+      content: "You cannot share an empty note!",
+      optionBuilder: () => {
+            'OK': null,
+          });
+}
